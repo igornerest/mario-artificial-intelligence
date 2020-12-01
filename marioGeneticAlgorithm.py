3578bba23f457ce32581973f343c1d9588bab03b
igornerest <igornerest@gmail.com>
2020-11-30 23:29:46 -0300

    Add training with crossover, mutation and selection

--- marioGeneticAlgorithm.py
+++ marioGeneticAlgorithm.py
@@ -9,7 +9,7 @@ from rominfo import *
 from utils import *
 import random
 import itertools
-
+import copy
 
 def format_state(state):
     return ','.join(map(lambda x : str(int(x)), state))  
@@ -19,22 +19,20 @@ possible_states = [format_state(s) for s in itertools.product([0, 1], repeat = 1
 mostrar = True
 
 class MarioAI:
-    def __init__(self, sensors = 10):
-        self.sensors = sensors
-        self.policy = self._generate_policy()
-        self.fitness = None
+    def __init__(self, sensors = 10, policy = None):
+        self.sensors, self.policy = sensors, policy
+        if self.policy == None:
+            self.policy = self._generate_policy()
+        self.fitness = self._calc_fitness()
 
     def get_policy(self):
-        return self.policy
+        return self.policy.copy()
 
     def get_fitness(self):
         if self.fitness == None:
             self.fitness = self._calc_fitness()
         
         return self.fitness
-
-    def update_policy(self, state, action):
-        self.policy[state] = action
 
     def _generate_policy(self):
         policy = dict()
@@ -49,25 +47,27 @@ class MarioAI:
         env = retro.make(game='SuperMarioWorld-Snes', state='YoshiIsland1', players=1)    
         env.reset()
         
+        max_it = 0
+        last_x_pos = 1
         fitness = 0
-        while not env.data.is_done(): 
+        while max_it < 50 and not env.data.is_done(): 
             state, x_pos, _ = self.get_current_state(env)
             action = self.evaluate(state)
             
             performAction(moves[action], env)
-
             fitness = max(fitness, x_pos)
-        
-            print(fitness)
+            max_it = max_it + 1 if last_x_pos == x_pos else 0
+            last_x_pos = x_pos
+
             if mostrar:
                 env.render()
-        
-
+                
+        print("calculated fitness: ", fitness)
         return fitness
 
     def evaluate(self, state):
-        print('state', state)
-        print('policy', self.policy[state])
+        #print('state', state)
+        #print('policy', self.policy[state])
         return self.policy[state]
 
     def get_current_state(self, env):
@@ -90,7 +90,7 @@ class MarioAI:
             'lower_right':  self.has_enemies(state, 8, 12, 8, 12),            
         }
 
-        print(format_state(transformed_states.values()))
+        #print(format_state(transformed_states.values()))
         return format_state(transformed_states.values()), x_pos, y_pos
 
     def has_enemies(self, state, col_beg, col_end, row_beg, row_end):
@@ -103,47 +103,99 @@ class MarioAI:
         return found_enemy
 
 class GeneticAlgorithm:
-    def __init__(self, generations = 100, population_size = 2):
+    def __init__(self, generations = 100, population_size = 5):
         self.population_size = population_size
         self.generations = generations
     
         self.population = self.generatePopulation()
+        self.best_fitness = 0
         #print(self.population)
 
     def generatePopulation(self):
         return [MarioAI() for _ in range(self.population_size)]
 
-    def crossover_individuals(self, ind_a, ind_b):
-        crossover_rate = random.randint(0, len(ind_a))
+    def _crossover(self, population):
+        print("Crossover")
+        removal_rate = int(0.2 * self.population_size)
+        target_p = self._remove_worst(population, removal_rate)
+
+        crossed_p = []
+        while len(crossed_p) < self.population_size:
+            [ind_a, ind_b] = random.sample(target_p, 2)
+            crossed_a, crossed_b = self._recombine_individuals(ind_a, ind_b)
+            crossed_p += [crossed_a, crossed_b]
         
-        policy_a = ind_a.policy
-        policy_b = ind_b.policy
+        return crossed_p
+
+    def _recombine_individuals(self, ind_a, ind_b):
+        crossover_rate = random.randint(0, len(possible_states))
         target_states = random.sample(possible_states, crossover_rate)
 
-        for ts in target_states:
-            ind_a.update_policy(ts, policy_b[ts])
-            ind_b.update_policy(ts, policy_a[ts])
+        policy_a = ind_a.get_policy()
+        policy_b = ind_b.get_policy()
 
-    def mutate_individual(self, individual):
+        for ts in target_states:
+            policy_a[ts], policy_b[ts] = policy_b[ts], policy_a[ts]
+
+        return MarioAI(policy = policy_a), MarioAI(policy = policy_b)
+
+    def _mutation(self, population):
+        print("mutation")
+        return [self._mutate_individual(ind) for ind in population]
+
+    def _mutate_individual(self, individual):
         target_state = random.choice(possible_states)
         mutated_action = random.choice(list(moves))
-        print(target_state, mutated_action)
-        individual.update_policy(target_state, mutated_action)
-    
-    def tournament(self, population):
-        pass
+        
+        policy = individual.get_policy()
+        policy[target_state] = mutated_action
 
-    def select_best(self, population, selection_rate):
-        pass
+        return MarioAI(policy = policy)
+    
+    def _selection(self, population):
+        print("selection")
+        selection_rate = int(0.2 * self.population_size)
+
+        selected = []
+        while len(selected) < self.population_size:
+            best_ind, population = self._select_best(population, selection_rate)
+            selected += best_ind
+            
+        return selected
+
+    def _select_best(self, population, selection_rate):
+        population.sort(key = lambda ind : ind.get_fitness(), reverse = True)
+        #print([p.get_fitness() for p in population])
+        return population[0:selection_rate], population[selection_rate:]
+
+    def _remove_worst(self, population, selection_rate):
+        population.sort(key = lambda ind : ind.get_fitness())
+        #print([p.get_fitness() for p in population])
+        return population[selection_rate:]
 
     def train(self):
-        pass
+        for gen in range(self.generations):
+            crossed = self._crossover(self.population)
+            mutate = self._mutation(self.population + crossed)
+            selected = self._selection(mutate)
+
+            best, _ = self._select_best(self.population, 1)
+            self.population = selected
+            self.best_fitness = best[0].get_fitness()
+            
+            print("Generation {0}. Best Fitness = {1}".format(gen, self.best_fitness))
+        #value = set(new_population[0].get_policy().items()) ^set(self.population[0].get_policy().items()) 
+
+        #print(value)
 
 def main():  
     global mostrar
     global env
   
     mostrar = True
+
+    ga = GeneticAlgorithm()
+    ga.train()
     
 if __name__ == "__main__":
     main()
